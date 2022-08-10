3ba8e9908784be5d923c2de7d427f627f115418c
marksolyom <solyom.mark@gmail.com>
2022-08-10 14:17:59 +0200

    add meme states and random img logic

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,7 +1,32 @@
+import React, { useState } from "react";
+import memesData from "../memesData.js";
+
 export default function Meme() {
+
+    const [meme, setMeme] = useState(
+        {
+            topText: "",
+            bottomText: "",
+            randomImg: "http://i.imgflip.com/1bij.jpg"
+        }
+    );
+
+    const [allMemeImgs, setAllMemeImgs] = useState(memesData);
+
+
+    function getMemeImg() {
+        const memesArray = allMemeImgs.data.memes;
+        const randomNumber = Math.floor(Math.random() * memesArray.length);
+        const url = memesArray[randomNumber].url;
+        setMeme(prevMeme => ({
+            ...prevMeme,
+            randomImg: url
+        }));
+    }
+
     return (
         <main>
-            <form>
+            <div className="form">
                 <input
                     type="text"
                     placeholder="Top text"
@@ -13,10 +38,12 @@ export default function Meme() {
                     className="form-input"
                 />
                 <button
-                className="form-btn">
-                    Get a new meme image <span className="weird-face">( ͡° ͜ʖ ͡°)</span>
+                    className="form-btn"
+                    onClick={getMemeImg}>
+                    GET RANDOM MEME
                 </button>
-            </form>
+            </div>
+            <img src={meme.randomImg} alt="meme" />
         </main>
     )
 }
